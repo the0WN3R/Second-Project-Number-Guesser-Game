af6f1b6ddc6a3c1531e77c5dc2aa1970b51219ca
the0WN3R <lukeockwood@gmail.com>
2025-04-24 08:44:33 -0700

    Create number_guesser.py
    Here it is! It was actually a lot easier than I though and took me less than 15 minutes.

---
+++ number_guesser.py
@@ -0,0 +1,18 @@
+import random
+
+x = random.choice(range(1, 100))
+print("Welcome to the number guessing game!")
+print("I have selected a number between 1 and 100.")
+for _ in range(5):
+    try:
+        y = int(input("What is the number? "))
+        if y == x:
+            print("You win!")
+            break
+        elif y < x:
+            print("Too low!")
+        else:
+            print("Too high!")
+    except ValueError:
+        print("Please enter a number betweeen 1 and 100.")
+print("Thank you for playing! The game is over.")
